2763627cc7e80a26682e884e7fda35f211017185
KennyHoang-CS <kennyhoangcs@gmail.com>
2021-03-17 23:39:50 -0700

    navbar is now responsive, media screens for 5 different device groups.

--- static/app.js
+++ static/app.js
@@ -1,3 +1,15 @@
-"use strict";
+// To toggle mobile menu
+const toggle = document.querySelector('.toggle');
+const menu = document.querySelector('.menu');
 
+function toggleMenu(){
+    if (menu.classList.contains('active')){
+        menu.classList.remove('active');
+        toggle.querySelector('a').innerHTML = "<i class='fas fa-bars fa-2x'></i>";
+    } else {
+        menu.classList.add("active")
+        toggle.querySelector('a').innerHTML = "<i class='fas fa-times fa-2x'></i>";
+    }
+}
 
+toggle.addEventListener('click', toggleMenu, false);
