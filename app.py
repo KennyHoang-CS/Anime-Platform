ef511380b4dfa34dd709509964b5fdee2468d73d
KennyHoang-CS <kennyhoangcs@gmail.com>
2021-03-22 17:09:38 -0700

    database config for heroku

--- app.py
+++ app.py
@@ -11,7 +11,9 @@ import os
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///anime_platform'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL',
+'postgresql:///anime_platform') 
+
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', my_secret_key)
